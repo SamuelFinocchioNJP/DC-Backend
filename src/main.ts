e16a72eb02bc4ca2761d649ed07d0364e11e35b6
Specy <specy.dev@gmail.com>
2022-09-26 23:45:44 +0200

    fixed issue with cors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,12 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }));
-
+  app.enableCors({
+    credentials: true,
+    origin: "http://localhost:3000",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    optionsSuccessStatus: 204
+  })
   await app.listen(8080);
 }
 bootstrap();
